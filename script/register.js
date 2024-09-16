6441e93d1203bbca1ee9bcb2c336c3e8453e06c1
shellyb-Online <shellybanahene@gmail.com>
2024-09-16 11:56:33 +0000

    Javascript variables and functions

---
+++ script/register.js
@@ -0,0 +1,10 @@
+// select form by id
+const registerForm = document.getElementById('register-form');
+// Handle form submit event
+registerForm.onsubmit = function() {
+    // Collect username
+    // Collect email
+    // Collect password
+    // Save user information
+    // Send confirmation email  
+}
