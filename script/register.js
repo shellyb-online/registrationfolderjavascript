4f351e477010fbec2c802a953ab1f3c14065b5f1
shellyb-Online <shellybanahene@gmail.com>
2024-09-17 14:39:04 +0000

    function added

--- script/register.js
+++ script/register.js
@@ -1,10 +1,23 @@
 // select form by id
-const registerForm = document.getElementById('register-form');
+const registerForm = document.querySelector('#register-form');
+console.log(registerForm);
 // Handle form submit event
-registerForm.onsubmit = function() {
+registerForm.addEventListener('submit', function () {
     // Collect username
     // Collect email
     // Collect password
     // Save user information
     // Send confirmation email  
+});
+
+// age variable
+const age = 44;
+// square of age
+function squareAge(age) {
+    const squareAge = age ** 2;
+    return squareAge;
 }
+
+console.log (squareAge (age));
+const squareAge = squareAge(45);
+console.log(squaredAge);
